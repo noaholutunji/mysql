9b75da927ad306fb55997e44ea1dc5761e974685
Noah Osuolale <osuolale49@gmail.com>
2019-05-23 23:46:10 +0100

    Weather Observation Challenge Solved

--- select-weather-observation-station-11/select.sql
+++ select-weather-observation-station-11/select.sql
@@ -1,19 +1,5 @@
-SELECT DISTINCT city 
+SELECT DISTINCT city
 FROM station 
-WHERE city NOT IN 
-(SELECT DISTINCT city 
-FROM station 
-WHERE city LIKE '%a' OR 
-city LIKE '%e' OR
-city LIKE '%i' OR  
-city LIKE '%o' OR 
-city LIKE '%u')
-OR 
-city NOT IN 
-(SELECT city 
-FROM station 
-WHERE city LIKE 'A%' OR 
-city LIKE 'E%' OR 
-city LIKE 'I%' OR 
-city LIKE 'O%' OR 
-city LIKE 'U%');
+WHERE SUBSTRING(city, 1,1) NOT IN ('A','E','I','O','U') 
+OR
+SUBSTRING(city, -1,1) NOT IN ('A','E','I','O','U');
--- select-weather-observation-station-8/select.sql
+++ select-weather-observation-station-8/select.sql
@@ -1,4 +1,4 @@
-SELECT city
+SELECT DISTINCT city
 FROM station 
 WHERE SUBSTRING(city, 1,1) IN ('A','E','I','O','U') 
 AND
