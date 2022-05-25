ee846fd357fd8249d99f09533024e7471ebbb2f8
Noah Osuolale <osuolale49@gmail.com>
2022-05-25 15:26:02 +0100

    Solved Symmetric Challenge

--- projects/select.sql
+++ projects/select.sql
@@ -1,6 +1,8 @@
-SELECT students.name FROM STUDENTS
-JOIN Friends ON students.id = friends.id
-JOIN Packages AS sp ON students.id = sp.id
-JOIN Packages AS fp ON friends.friend_id = fp.id
-WHERE sp.salary < fp.salary
-ORDER BY fp.salary;
+set sql_mode = "STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION";
+
+SELECT Start_Date, End_Date FROM
+(SELECT Start_Date FROM Projects WHERE Start_Date NOT IN (SELECT End_Date FROM Projects)) AS s,
+(SELECT End_Date FROM Projects WHERE End_Date NOT IN (SELECT Start_Date FROM Projects)) AS e
+WHERE Start_Date < End_Date
+GROUP BY Start_Date
+ORDER BY DATEDIFF(End_Date, Start_Date), Start_Date;
---
+++ symmetric-pair/README.md
@@ -0,0 +1,10 @@
+# Symmetric Pair
+
+You are given a table, Functions, containing two columns: X and Y.
+
+![Functions Table](https://s3.amazonaws.com/hr-challenge-images/12892/1443818798-51909e977d-1.png)
+
+Two pairs (X1, Y1) and (X2, Y2) are said to be symmetric pairs if X1 = Y2 and X2 = Y1.
+
+Write a query to output all such symmetric pairs in ascending order by the value of X.
+
